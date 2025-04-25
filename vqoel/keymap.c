911af9367afd90f5a4f5022672fdee3733662101
kpeifer <111024414+kpeifer@users.noreply.github.com>
2025-04-25 11:37:23 -0500

    Update keymap.c

--- vqoel/keymap.c
+++ vqoel/keymap.c
@@ -364,7 +364,7 @@ void dance_2_finished(tap_dance_state_t *state, void *user_data) {
     switch (dance_state[2].step) {
         case SINGLE_TAP: register_code16(KC_SPACE); break;
         case SINGLE_HOLD: register_code16(KC_RIGHT_SHIFT); break;
-        case DOUBLE_TAP: register_code16(KC_DOT); break;
+        case DOUBLE_TAP: register_code16(KC_DOT); register_code16(KC_SPACE); break;
         case DOUBLE_SINGLE_TAP: tap_code16(KC_SPACE); register_code16(KC_SPACE);
     }
 }
@@ -374,7 +374,7 @@ void dance_2_reset(tap_dance_state_t *state, void *user_data) {
     switch (dance_state[2].step) {
         case SINGLE_TAP: unregister_code16(KC_SPACE); break;
         case SINGLE_HOLD: unregister_code16(KC_RIGHT_SHIFT); break;
-        case DOUBLE_TAP: unregister_code16(KC_DOT); break;
+        case DOUBLE_TAP: unregister_code16(KC_DOT); unregister_code16(KC_SPACE);  break;
         case DOUBLE_SINGLE_TAP: unregister_code16(KC_SPACE); break;
     }
     dance_state[2].step = 0;
